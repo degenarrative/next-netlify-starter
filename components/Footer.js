27990f167f1d29ab7ad085b6fd44c3bac07d620e
degenarrative <98234022+degenarrative@users.noreply.github.com>
2022-01-22 15:25:37 -0800

    Adding new Discord link

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,7 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         Twitter <a href="https://twitter.com/degenarrative"><img src="/twitter.svg" alt="Twitter Logo" className={styles.logo} /></a>
+        Discord <a href="https://discord.gg/F3vteyNEpx"><img src="/discord.svg" alt="Discord Logo" className={styles.logo} /></a>
       </footer>
     </>
   )
