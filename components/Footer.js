a0cf46f75e65acb5dbc55aff2b0e4e2824f06b6c
degenarrative <98234022+degenarrative@users.noreply.github.com>
2022-01-22 15:02:49 -0800

    Fixing build error

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,6 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         Twitter <a href="https://twitter.com/degenarrative"><img src="/twitter.svg" alt="Twitter Logo" className={styles.logo} /></a>
-        <!--Discord <a href="https://discord.gg/ZStFS8cGUf"><img src="/discord.svg" alt="Discord Logo" className={styles.logo} /></a>-->
       </footer>
     </>
   )
