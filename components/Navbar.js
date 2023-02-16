9651b87f807045dd17d169c0e56bd50f6cfa1175
degenarrative <98234022+degenarrative@users.noreply.github.com>
2023-02-16 09:32:57 -0800

    Update navbar js

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,45 +3,42 @@ import Link from 'next/link';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-  function toggleNavbar() {
-    setIsOpen(!isOpen);
-  }
+  const handleClick = () => {
+    setOpen(!open);
+  };
 
   return (
-    <nav className={styles.navbar}>
-      <Link href="/">
-        <a className={styles.navbarBrand}>Degenarrative</a>
-      </Link>
-      <button className={styles.hamburger} onClick={handleClick}>
-        <div className={open ? styles.line1open : styles.line1}></div>
-        <div className={open ? styles.line2open : styles.line2}></div>
-        <div className={open ? styles.line3open : styles.line3}></div>
-      </button>
-      <ul
-        className={`${styles.navbarNav} ${isOpen ? styles.open : ''}`}
-        onClick={toggleNavbar}
-      >
-        <li className={styles.navItem}>
-          <Link href="/">
-            <a className={styles.navLink}>Home</a>
-          </Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/about">
-            <a className={styles.navLink}>About</a>
-          </Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/contact">
-            <a className={styles.navLink}>Contact</a>
-          </Link>
-        </li>
-      </ul>
+    <nav className={styles.nav}>
+      <div className={styles.container}>
+        <button className={styles.hamburger} onClick={handleClick}>
+          <div className={open ? styles.line1open : styles.line1}></div>
+          <div className={open ? styles.line2open : styles.line2}></div>
+          <div className={open ? styles.line3open : styles.line3}></div>
+        </button>
+        <ul className={`${styles.navLinks} ${open ? styles.navOpen : ''}`}>
+          <li className={styles.navLink}>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li className={styles.navLink}>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          <li className={styles.navLink}>
+            <Link href="/contact">
+              <a>Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
+
 
 
 
