7a575a4bc313e3f5877126a67d1c557e81f87adf
degenarrative <98234022+degenarrative@users.noreply.github.com>
2023-02-16 12:54:35 -0800

    Simple Nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,42 +1,24 @@
-import { useState } from 'react';
 import Link from 'next/link';
-import styles from './Navbar.module.css';
 
-export default function Navbar({ title }) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
-
+export default function Navbar() {
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.navbarTitle}>{title}</div>
-      <button className={styles.hamburger} onClick={handleToggle}>
-        <span className={styles.hamburgerBox}>
-          <span className={styles.hamburgerInner}></span>
-        </span>
-      </button>
-      <ul className={`${styles.navLinks} ${isOpen ? styles.open : ''}`}>
-        <li className={styles.navLink}>
+    <nav>
+      <ul>
+        <li>
           <Link href="/">
             <a>Home</a>
           </Link>
         </li>
-        <li className={styles.navLink}>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-        <li className={styles.navLink}>
-          <Link href="/contact">
-            <a>Contact</a>
+        <li>
+          <Link href="/signup">
+            <a>Sign Up</a>
           </Link>
         </li>
       </ul>
     </nav>
   );
 }
+
 
 
 
