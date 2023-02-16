d3d17dfd5931a8da23fb6d376b5b8d0c1256474c
degenarrative <98234022+degenarrative@users.noreply.github.com>
2023-02-16 09:15:53 -0800

    Update navbar js

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,41 +3,41 @@ import Link from 'next/link';
 import styles from './Navbar.module.css';
 
 export default function Navbar({ title }) {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  function toggleMenu() {
-    setIsOpen((prevIsOpen) => !prevIsOpen);
+  function handleMenuToggle() {
+    setIsMenuOpen(!isMenuOpen);
   }
 
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.brand}>
-        <Link href="/">
-          <a>{title}</a>
-        </Link>
-      </div>
-
-      <button className={styles.toggle} onClick={toggleMenu}>
-        <span className={styles.hamburger} aria-hidden="true"></span>
-      </button>
-
-      <ul className={`${styles.menu} ${isOpen ? styles.showMenu : ''}`}>
-        <li>
+    <nav className={styles.nav}>
+      <div className={styles.navbar}>
+        <div className={styles.brand}>
           <Link href="/">
-            <a>Home</a>
+            <a>{title}</a>
           </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-            <a>Contact</a>
-          </Link>
-        </li>
-      </ul>
+        </div>
+        <button className={styles.toggle} onClick={handleMenuToggle}>
+          <span className={styles.toggleIcon} aria-hidden="true"></span>
+        </button>
+        <ul className={`${styles.menu} ${isMenuOpen ? styles.open : ''}`}>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a>Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
