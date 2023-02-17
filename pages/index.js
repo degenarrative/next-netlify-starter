e6973a4c8b6725c4dd4f747fede8c3899b5d90ac
degenarrative <98234022+degenarrative@users.noreply.github.com>
2023-02-17 15:16:25 -0800

    Update index js

--- pages/index.js
+++ pages/index.js
@@ -73,7 +73,6 @@ export default function Home() {
             This is the NERD War; a struggle for the soul of humanity, fought with electrons in the neon-lit alleyways of cyberspace. Fire and code, blood and the pulse of binary. The streets of the future boil over as the NERDs and Corporations clash in a conflict that will define their generation.
             </p>
           </div>
-         </div>
         </div>
         <div className="btn-container">
           <a href="#nerd" className="btn btn-primary" onClick={scrollToNerdSection}>NERD</a>
