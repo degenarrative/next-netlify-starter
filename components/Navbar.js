5af12616acf8818e1bda004a1c09d89a82200499
degenarrative <98234022+degenarrative@users.noreply.github.com>
2023-02-17 14:23:54 -0800

    Update Navbar js

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,6 +2,12 @@ import Link from 'next/link';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
+  function scrollToNerdSection(event) {
+    event.preventDefault();
+    const nerdSection = document.getElementById('nerd-section');
+    nerdSection.scrollIntoView({ behavior: 'smooth' });
+  }
+
   return (
     <nav className={styles.nav}>
       <ul className={styles.navList}>
@@ -10,15 +16,13 @@ export default function Navbar() {
             <a className={styles.navLink}>degenarrative</a>
           </Link>
         </li>
-        <li>
-          <Link href="#nerd">
-            <a className={styles.navLink}>NERD</a>
-          </Link>
-        </li>
         <li className={styles.navListItem}>
           <Link href="https://coda.io/form/Neo-Elite-Radical-Degens_dtwh87bz2h-">
             <a className={styles.navButton}>Sign Up</a>
           </Link>
+        </li>
+        <li className={styles.navListItem}>
+          <a href="#" onClick={scrollToNerdSection} className={styles.navLink}>NERD</a>
         </li>
       </ul>
     </nav>
