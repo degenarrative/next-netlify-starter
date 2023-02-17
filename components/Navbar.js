c927554f2d8f3d780e3712fb5420dbb82d4f608d
degenarrative <98234022+degenarrative@users.noreply.github.com>
2023-02-17 14:48:40 -0800

    Update Navbar js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,28 @@
 import Link from 'next/link';
+import { useState } from 'react';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
+  const [isSticky, setIsSticky] = useState(false);
+
   function scrollToNerdSection(event) {
     event.preventDefault();
     const nerdSection = document.getElementById('nerd-section');
     nerdSection.scrollIntoView({ behavior: 'smooth' });
   }
 
+  function handleScroll() {
+    const offset = window.pageYOffset;
+    setIsSticky(offset > 0);
+  }
+
+  // Attach scroll event listener
+  if (typeof window !== 'undefined') {
+    window.addEventListener('scroll', handleScroll);
+  }
+
   return (
-    <nav className={styles.nav}>
+    <nav className={`${styles.nav} ${isSticky ? styles.sticky : ''}`}>
       <ul className={styles.navList}>
         <li>
           <Link href="/">
@@ -17,7 +30,7 @@ export default function Navbar() {
           </Link>
         </li>
         <li className={styles.navListItem}>
-          <a href="#nerd-section" onClick={scrollToNerdSection} className={styles.navLink}>NERD</a>
+          <a href="#" onClick={scrollToNerdSection} className={styles.navLink}>NERD</a>
         </li>
         <li className={styles.navListItem}>
           <Link href="https://coda.io/form/Neo-Elite-Radical-Degens_dtwh87bz2h-">
@@ -28,7 +41,6 @@ export default function Navbar() {
     </nav>
   );
 }
-
 
 
 
