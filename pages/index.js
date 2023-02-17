62048b4cee77126c0d96defa6c3aa39dfb7c2ed6
degenarrative <98234022+degenarrative@users.noreply.github.com>
2023-02-16 16:44:43 -0800

    Update index js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Navbar />
+      <Navbar/>
 
       <main>
         <div className="main-content wrapper">
