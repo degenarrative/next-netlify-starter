33f0a6021773a6e1b0f17cc09b312a21f3e7369e
degenarrative <98234022+degenarrative@users.noreply.github.com>
2023-02-17 16:21:12 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
         <div className="btn-container">
           <a href="/join-list" className="btn btn-primary">Neo-Elite Radical Degens</a>
         </div>
-        <div className="banner">
+        <div className="banner banner-container">
           <div className="banner-image">
             <img src="/NERD_banner.png" alt="NERD Banner Image" />
           </div>
