f0d863f6a46db16d5ea3fc9684eeaf32ea25d6dd
degenarrative <98234022+degenarrative@users.noreply.github.com>
2023-03-04 10:44:19 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,11 @@ export default function Home() {
 
       <main>
         <Header title="...degenarrative..." />
+          <div className="banner banner-container">
+          <div className="banner-image">
+            <img src="/degenarrative_banner.png" alt="Degenarrative Banner Image" />
+          </div>        
+        </div>
         <div className="main-content wrapper">
           <div className="main-text">
             <p>
